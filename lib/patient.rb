c007b486e895250d6da0dc27a5adf3dce4a4f586
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-06-15 23:21:28 +0000

    automatically backed up by learn

---
+++ lib/patient.rb
@@ -0,0 +1,18 @@
+class Patient
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def new_appointment(doctor, date)
+    new_app = Appointment.new(self, doctor, date)
+  end
+
+  def appointments
+    Appointment.all.select {|i| i.patient == self}
+  end
+
+  def doctor
+    appointments.map {|i| i.doctor}
+  end
