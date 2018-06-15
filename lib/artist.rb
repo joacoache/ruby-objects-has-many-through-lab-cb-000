dbe7fe0cb7b6be425e40a38d3ac69f195c7a6835
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-06-15 22:52:12 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,24 @@
+class Artist
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def new_song(name, genre)
+    song = Song.new(name, genre)
+  end
+
+  def songs
+    Song.all.select do |songs|
+      songs.artist == self.name
+    end
+  end
+
+  def genre
+    songs.map do |song|
+      song.genre
+    end
+  end
+
+end
